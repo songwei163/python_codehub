cf4cb94505abcb2464c3d23e36410883b7ea5cb0
songwei163 <songweicpp@163.com>
2020-09-22 15:53:21 +0800

    1.开始一个新的PyGame项目;2.开启游戏主循环，绘制屏幕对象，设置窗口尺寸和颜色，监测退出事件，创建Settings类;

---
+++ Game/settings.py
@@ -0,0 +1,10 @@
+# 用于设置的类
+
+class Settings:
+    def __init__(self):
+        # 窗口宽度
+        self.screen_width = 1200
+        # 窗口高度
+        self.screen_height = 800
+        # 窗口背景颜色
+        self.bg_color = (230, 230, 230)
